2843fef48a89291e24c4dfe0013ee98253c6c8d7
Danielk10 <danielpdiamon@gmail.com>
2024-10-08 19:26:00 -0400

    update

--- app/src/androidTest/java/com/naarasalonyspa/naara/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/naarasalonyspa/naara/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.diamon.guia;
+package com.naarasalonyspa.naara;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.diamon.guia", appContext.getPackageName());
+        assertEquals("com.naarasalonyspa.naara", appContext.getPackageName());
     }
 }
