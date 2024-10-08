7f700012058294e419d813f1a596d6124f5976ca
Danielk10 <danielpdiamon@gmail.com>
2024-10-08 19:39:46 -0400

    update

--- app/src/main/java/com/naarasalonyspa/naara/MainActivity.java
+++ app/src/main/java/com/naarasalonyspa/naara/MainActivity.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        AppCenter.start(getApplication(), "447c7f85-0c9e-4470-a7b3-86c55255b873",
+                                  Analytics.class, Crashes.class);
   
         setContentView(R.layout.activity_main);
      
