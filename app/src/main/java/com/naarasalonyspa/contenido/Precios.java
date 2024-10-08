e56407561ab12271233b91b6c1827dc98817a7b4
Danielk10 <danielpdiamon@gmail.com>
2024-10-08 18:56:41 -0400

    Correcion

---
+++ app/src/main/java/com/naarasalonyspa/contenido/Precios.java
@@ -0,0 +1,37 @@
+package com.naarasalonyspa.contenido;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.content.Context;
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Precios extends Fragment {
+  
+    private String sectionName;
+    private Context contexto;
+
+
+    public Precios(Context contexto, String sectionName) {
+      
+        this.contexto = contexto;
+        this.sectionName = sectionName;
+      
+    }
+
+    @Nullable
+    @Override
+    public android.view.View onCreateView(@NonNull android.view.LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(contexto);
+        textView.setText(sectionName);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(24);
+        return textView;
+    }
+}
---
+++ app/src/main/java/com/naarasalonyspa/naara/MainActivity.java
@@ -0,0 +1,93 @@
+package com.naarasalonyspa.naara;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import androidx.fragment.app.Fragment;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import com.naarasalonyspa.diseno.SectionsPagerAdapter;
+import com.naarasalonyspa.contenido.Inicio;
+import com.naarasalonyspa.contenido.AcercaDe;
+import com.naarasalonyspa.contenido.Precios;
+import com.naarasalonyspa.contenido.Servicios;
+import com.naarasalonyspa.contenido.Contacto;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AppCenter.start(getApplication(), "447c7f85-0c9e-4470-a7b3-86c55255b873",
+                          Analytics.class, Crashes.class);
+       /*
+        // Crear ViewPager de manera programática
+        ViewPager viewPager = new ViewPager(this);
+        viewPager.setId(View.generateViewId());
+
+        // Crear lista de fragmentos para cada sección
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new Inicio(this,"Inicio"));
+        fragments.add(new AcercaDe(this,"Acerca de"));
+        fragments.add(new Servicios(this,"Servicios"));
+        fragments.add(new Precios(this,"Precios"));
+        fragments.add(new Contacto(this,"Contacto"));
+
+        // Configurar adaptador del ViewPager
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager(), fragments);
+        viewPager.setAdapter(adapter);*/
+        
+        
+ // UI programática
+        TextView textView = new TextView(this);
+        textView.setText("Bienvenido a Naara Salón & Spa");
+        textView.setTextSize(24);
+        textView.setGravity(Gravity.CENTER);
+        
+        
+      setContentView(textView);
+
+    }
+/*    
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+        // Agregar elementos del menú programáticamente
+        MenuItem menuServicios = menu.add(Menu.NONE, 1, 1, "Servicios");
+        MenuItem menuContacto = menu.add(Menu.NONE, 2, 2, "Contacto");
+
+        // Configurar acciones de los elementos
+        menuServicios.setOnMenuItemClickListener(item -> {
+            Toast.makeText(this, "Servicios seleccionados", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+
+        menuContacto.setOnMenuItemClickListener(item -> {
+            Toast.makeText(this, "Contacto seleccionado", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+
+        return true;
+    }*/
+}
