7617f40ccbcc063199c388e8c9f4c2dd153be37c
Danielk10 <danielpdiamon@gmail.com>
2024-10-09 15:18:20 -0400

    update

--- app/src/main/java/com/naarasalonyspa/servidor/ImageDownloaderTask.java
+++ app/src/main/java/com/naarasalonyspa/servidor/ImageDownloaderTask.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.InputStream;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
